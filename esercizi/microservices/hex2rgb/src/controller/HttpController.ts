a01ee53b0e5bb8a79ca53af462d42ae301c70707
Paolo <50113488+paolinux97@users.noreply.github.com>
2020-03-17 19:10:25 +0100

    configurazione iniziale

---
+++ esercizi/microservices/hex2rgb/src/controller/HttpController.ts
@@ -0,0 +1,16 @@
+import {convert} from '../service/Service';
+import {Express} from 'express';
+
+//TODO
+class HttpController {
+    constructor(server: Express) {
+        server.get('/', (req, res) => {
+            const color = JSON.parse(req.query.color) as ColorModel;
+            const convertedColor: ColorModel = convert(color);
+
+            res.send(convertedColor);
+        });
+    }
+}
+
+export default HttpController;
